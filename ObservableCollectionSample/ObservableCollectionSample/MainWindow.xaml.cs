4803b4170ea16a7d0d4a17fb8e96f5333c6529eb
beryah <sgm4320@gmail.com>
2015-05-29 21:30:31 +0800

    Create ObservableCollectionSample.sln.

---
+++ ObservableCollectionSample/ObservableCollectionSample/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace ObservableCollectionSample
+{
+  /// <summary>
+  /// MainWindow.xaml 的互動邏輯
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      var vm = new ViewModel();
+
+      vm.Add = t =>
+      {
+        this.Dispatcher.Invoke(() =>
+        {
+          vm.Items.Add(t);
+        });
+      };
+
+      this.DataContext = vm;
+
+      await vm.GoAsync();
+    }
+  }
+}
---
+++ ObservableCollectionSample/ObservableCollectionSample/ViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace ObservableCollectionSample
+{
+  public class ViewModel : INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private ObservableCollection<string> items = new ObservableCollection<string>();
+
+    public ObservableCollection<string> Items
+    {
+      get { return items; }
+      set
+      {
+        items = value;
+        OnPropertyChanged();
+      }
+    }
+
+    public Action<string> Add { get; set; }
+
+    public async Task GoAsync()
+    {
+      foreach (var i in Enumerable.Range(0, 20000))
+      {
+        await Task.Delay(100);
+        Add(i.ToString());
+      }
+    }
+
+    private void OnPropertyChanged([CallerMemberName]string name = "")
+    {
+      if (PropertyChanged != null)
+      {
+        PropertyChanged(this, new PropertyChangedEventArgs(name));
+      }
+    }
+  }
+}
