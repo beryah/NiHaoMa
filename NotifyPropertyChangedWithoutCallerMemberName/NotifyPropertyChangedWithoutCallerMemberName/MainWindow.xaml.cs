7997fbf5339bc8c6af719e28ac064c15ba287d4e
beryah <sgm4320@gmail.com>
2015-06-04 16:42:17 +0800

    NotifyPropertyChangedWithoutCallerMemberName

---
+++ NotifyPropertyChangedWithoutCallerMemberName/NotifyPropertyChangedWithoutCallerMemberName/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace NotifyPropertyChangedWithoutCallerMemberName
+{
+  /// <summary>
+  /// MainWindow.xaml 的互動邏輯
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+      var vm = new ViewModel();
+
+      this.DataContext = vm;
+
+      vm.Loaded();
+    }
+  }
+}
---
+++ NotifyPropertyChangedWithoutCallerMemberName/NotifyPropertyChangedWithoutCallerMemberName/ViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+
+namespace NotifyPropertyChangedWithoutCallerMemberName
+{
+  public class ViewModel : INotifyPropertyChanged
+  {
+    private string _text;
+
+    public string TextXX
+    {
+      get { return _text; }
+      set
+      {
+        _text = value;
+        OnPropertyChanged(() => TextXX);
+      }
+    }
+
+    public void Loaded()
+    {
+      TextXX = "Hello!!";
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    private void OnPropertyChanged(string propertyName)
+    {
+      if (PropertyChanged != null)
+      {
+        PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+      }
+    }
+
+    private void OnPropertyChanged(Func<object> func)
+    {
+      var propertyName = func.Method.Name.Split(new[] { "<set_", ">" }, StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
+
+      if (!string.IsNullOrEmpty(propertyName))
+      {
+        OnPropertyChanged(propertyName);
+      }
+    }
+  }
+}
